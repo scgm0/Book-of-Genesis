af263e8f3712072d31733445242f9b524c0db504
scgm0 <2682963017@qq.com>
2024-01-05 05:20:39 +0800

    清理无用插件

--- addons/nklbdev.importality/standalone_image_format_loader_extension.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends ImageFormatLoaderExtension
-
-const _Setting = preload("setting.gd")
-
-func get_settings() -> Array[_Setting]:
-	assert(false, "This method is abstract and must be overriden.")
-	return []
