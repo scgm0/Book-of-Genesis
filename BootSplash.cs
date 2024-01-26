e84ef1dcd6eeb4c6bd70429a04b8910602db5316
scgm0 <2682963017@qq.com>
2024-01-27 06:54:53 +0800

    游戏：添加动态启动画面

---
+++ BootSplash.cs
@@ -0,0 +1,29 @@
+using Godot;
+
+namespace 创世记;
+
+public partial class BootSplash : Control {
+	[GetNode("%Logo")] private TextureRect _logo;
+
+	[Export(PropertyHint.File, "*.tscn")] private string _scenePath;
+
+	public override void _Ready() {
+		Control? instancedScene = null;
+		_logo.Modulate = _logo.Modulate with { A = 0 };
+		ResourceLoader.LoadThreadedRequest(_scenePath);
+		var tween = CreateTween();
+		tween.SetTrans(Tween.TransitionType.Cubic);
+		tween.TweenProperty(_logo, "modulate:a", 1.0, 1.5);
+		tween.TweenCallback(Callable.From(() => {
+			var packedScene = ResourceLoader.LoadThreadedGet(_scenePath) as PackedScene;
+			instancedScene = packedScene?.Instantiate<Control>();
+			if (instancedScene == null) return;
+			instancedScene.Visible = false;
+			AddSibling(instancedScene);
+			Utils.Tree.Root.MoveChild(instancedScene, -2);
+		}));
+		tween.TweenCallback(Callable.From(() => { instancedScene!.Visible = true; })).SetDelay(0.05);
+		tween.Parallel().TweenProperty(this, "modulate:a", 0, 1);
+		tween.TweenCallback(Callable.From(QueueFree));
+	}
+}
