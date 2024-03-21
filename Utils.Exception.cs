87553fb90b2b2ff5f624a75e8a27691f0b8141e3
scgm0 <2682963017@qq.com>
2024-03-21 10:32:11 +0800

    C#：改进`JsValueToVariant`和`VariantToJsValue`方法 游戏：世界列表控件微调

--- Utils.Exception.cs
+++ Utils.Exception.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Numerics;
 using System.Text;
 using System.Threading;
 using Godot;
+using Godot.Collections;
 using Jint;
 using Jint.Native;
+using Jint.Native.Function;
 using Jint.Runtime;
 
 namespace 创世记;
@@ -30,13 +33,61 @@ public static partial class Utils {
 		return index > -1 ? str.Remove(index, oldValue.Length).Insert(index, newValue) : str;
 	}
 
+	enum Js2DicType {
+		BigInt,
+		Date
+	}
+
 	public static Variant JsValueToVariant(this JsValue jsValue) {
 		switch (jsValue.Type) {
 			case Types.Boolean: return jsValue.AsBoolean();
 			case Types.String: return jsValue.AsString();
-			case Types.BigInt: return (int)jsValue.AsNumber();
+			case Types.BigInt: {
+				var dic = new Dictionary();
+				dic["Js2DicType"] = (int)Js2DicType.BigInt;
+				dic["value"] = TypeConverter.ToBigInt(jsValue).ToByteArray();
+				return dic;
+			}
 			case Types.Number: return jsValue.AsNumber();
-			case Types.Object: return Json.ParseString(JsonSerializer!.Serialize(jsValue).AsString());
+			case Types.Object: {
+				if (jsValue is Function) return default;
+
+				if (jsValue.IsFloat32Array()) {
+					return jsValue.AsFloat32Array();
+				}
+
+				if (jsValue.IsFloat64Array()) {
+					return jsValue.AsFloat64Array();
+				}
+
+				if (jsValue.IsInt32Array() || jsValue.IsUint32Array() || jsValue.IsInt16Array() || jsValue.IsUint16Array() || jsValue.IsInt8Array()) {
+					return jsValue.AsInt32Array();
+				}
+
+				if (jsValue.IsUint8Array() || jsValue.IsUint8ClampedArray()) {
+					return jsValue.AsUint8Array();
+				}
+
+				if (jsValue.IsBigInt64Array() || jsValue.IsBigUint64Array()) {
+					return jsValue.AsBigInt64Array();
+				}
+
+				var dic = new Dictionary();
+
+				if (jsValue.IsDate()) {
+					dic["Js2DicType"] = (int)Js2DicType.Date;
+					dic["value"] = jsValue.AsDate().DateValue;
+					return dic;
+
+				}
+
+				var obj = jsValue.AsObject();
+				foreach (var (key, propertyDescriptor) in obj.GetOwnProperties()) {
+					dic[key.JsValueToVariant()] = propertyDescriptor.Value.JsValueToVariant();
+				}
+
+				return dic;
+			}
 			case Types.Symbol:
 			case Types.Empty:
 			case Types.Undefined:
@@ -52,8 +103,60 @@ public static partial class Utils {
 			case Variant.Type.Float: return variant.AsDouble();
 			case Variant.Type.String:
 			case Variant.Type.StringName: return variant.AsString();
-			case Variant.Type.Dictionary:
-			case Variant.Type.Array: return JsonParser!.Parse(Json.Stringify(variant));
+			case Variant.Type.Dictionary: {
+				var dic = variant.AsGodotDictionary();
+				if (dic.TryGetValue("Js2DicType", out var type) && type.VariantType == Variant.Type.Int) {
+					switch ((int)type) {
+						case (int)Js2DicType.BigInt: {
+							var bytes = dic["value"].AsByteArray();
+							return new JsBigInt(new BigInteger(bytes));
+						}
+						case (int)Js2DicType.Date: {
+							var date = dic["value"].AsInt64();
+							return new JsDate(Main.CurrentEngine!, date);
+						}
+					}
+				}
+
+				var obj = new JsObject(Main.CurrentEngine!);
+				foreach (var (key, value) in dic) {
+					obj[key.VariantToJsValue()] = value.VariantToJsValue();
+				}
+
+				return obj;
+			}
+			case Variant.Type.Array: {
+				var jsArray = new JsArray(Main.CurrentEngine!);
+				var array = variant.AsGodotArray();
+				foreach (var item in array) {
+					jsArray.Push(item.VariantToJsValue());
+				}
+
+				return jsArray;
+			}
+			case Variant.Type.PackedByteArray: {
+				var bytes = variant.AsByteArray();
+				return Main.CurrentEngine!.Intrinsics.Uint8Array.Construct(bytes);
+			}
+			case Variant.Type.PackedInt32Array: {
+				var bytes = variant.AsInt32Array();
+				return Main.CurrentEngine!.Intrinsics.Int32Array.Construct(bytes);
+			}
+			case Variant.Type.PackedInt64Array: {
+				var bytes = variant.AsInt64Array();
+				return Main.CurrentEngine!.Intrinsics.BigInt64Array.Construct(bytes);
+
+			}
+			case Variant.Type.PackedFloat32Array: {
+				var bytes = variant.AsFloat32Array();
+				return Main.CurrentEngine!.Intrinsics.Float32Array.Construct(bytes);
+
+			}
+			case Variant.Type.PackedFloat64Array: {
+				var bytes = variant.AsFloat64Array();
+				return Main.CurrentEngine!.Intrinsics.Float64Array.Construct(bytes);
+
+			}
 			case Variant.Type.Object:
 			case Variant.Type.Nil:
 			case Variant.Type.Vector2:
@@ -76,11 +179,6 @@ public static partial class Utils {
 			case Variant.Type.Rid:
 			case Variant.Type.Callable:
 			case Variant.Type.Signal:
-			case Variant.Type.PackedByteArray:
-			case Variant.Type.PackedInt32Array:
-			case Variant.Type.PackedInt64Array:
-			case Variant.Type.PackedFloat32Array:
-			case Variant.Type.PackedFloat64Array:
 			case Variant.Type.PackedStringArray:
 			case Variant.Type.PackedVector2Array:
 			case Variant.Type.PackedVector3Array:
